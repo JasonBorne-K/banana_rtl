449bc3fc240713f395fc5e6273dc638d71cbe050
JasonBorne-K <75062436+JasonBorne-K@users.noreply.github.com>
2020-12-08 12:04:00 +0900

    Update sh_Banana.sh
    change to A_3MB

--- sh_Banana.sh
+++ sh_Banana.sh
@@ -2,7 +2,7 @@
 
 
 
-mv 3MB.mdt R_3MB_1.mdt
+mv 3MB.mdt A_3MB_1.mdt
 
 cnt=1
 old=1
@@ -10,15 +10,15 @@ old=1
 while true
 do
 
-echo R_3MB_"$old".mdt
-curl -T ./R_3MB_"$old".mdt -a -u RTL:seba12345! ftp://ebrave.iptime.org:12121 -m 60 
+echo A_3MB_"$old".mdt
+curl -T ./A_3MB_"$old".mdt -a -u RTL:seba12345! ftp://ebrave.iptime.org:12121 -m 60 
 if [ $? -ne 0 ]; then
 break;
 fi
 
 cnt=$((cnt+1))
 
-mv R_3MB_"$old".mdt R_3MB_"$cnt".mdt
+mv A_3MB_"$old".mdt A_3MB_"$cnt".mdt
 old=$((old+1))
 #sleep 1
 if [ $old -eq 10 ]; then
@@ -27,6 +27,6 @@ fi
 
 done
 
-mv R_3MB_"${old}".mdt 3MB.mdt
+mv A_3MB_"${old}".mdt 3MB.mdt
 
 echo "Finished!!!"
